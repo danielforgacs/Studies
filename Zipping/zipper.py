a65fa26afcd474e04de33db1f4d0f9cf7f62c884
df@ <@>
2020-08-15 13:57:27 +0100

    works, but saves full path instead of relative

---
+++ Zipping/zipper.py
@@ -0,0 +1,20 @@
+import os
+import zipfile
+
+
+rootdir = os.path.dirname(__file__)
+archivename = os.path.join(rootdir, 'stuff.zip')
+
+files = [
+    'a.txt',
+    'b.txt',
+]
+
+print(rootdir)
+print(archivename)
+
+
+with zipfile.ZipFile(archivename, 'w') as zipf:
+    for fname in files:
+        fpath = os.path.join(rootdir, fname)
+        zipf.write(fpath)
