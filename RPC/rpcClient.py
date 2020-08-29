5b7ae99cf684f949ffedba4f4c3c3c164d7e743c
df@ <@>
2020-08-29 17:43:01 +0100

    experiment

--- RPC/rpcClient.py
+++ RPC/rpcClient.py
@@ -1,19 +1,20 @@
 import xmlrpc.client
 
+
 server = xmlrpc.client.ServerProxy(
     uri='http://localhost:8000',
     allow_none=True,
     use_builtin_types=True,
 )
-print(server.system.listMethods())
 
-response = server.fakit()
-print(response)
+stuff = [
+    'pk',
+    123,
+    {
 
-print(type(server.CoolClass))
+    }
+]
 
-insta = server.CoolClass()
-print(type(insta))
-# insta2 = server.CoolClass(pk=1)
-print(insta)
-# print(insta2.pk)
+response = server.do_something(stuff)
+response = server.do_something(stuff)
+response = server.do_something(stuff)
--- RPC/rpcServer.py
+++ RPC/rpcServer.py
@@ -2,52 +2,24 @@ from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
 
-def fakit():
-    text = 'Heeeeyyy.... :)s'
-    print(text)
 
-    return text
+def do_something(stuffs):
+    print(stuffs)
 
 
 
-class CoolClass:
-    def __init__(self, pk=None):
-        self.pk = pk
-
-
-
-# Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-# Create server
+
+
 with SimpleXMLRPCServer(
-        ('localhost', 8000),
+        addr=('localhost', 8000),
         requestHandler=RequestHandler,
         allow_none = True,
         use_builtin_types=True,
     ) as server:
+
     server.register_introspection_functions()
-
-    # Register pow() function; this will use the value of
-    # pow.__name__ as the name, which is just 'pow'.
-    server.register_function(pow)
-    server.register_function(fakit)
-    server.register_function(CoolClass)
-
-    # Register a function under a different name
-    def adder_function(x, y):
-        return x + y
-
-    server.register_function(adder_function, 'add')
-
-    # Register an instance; all the methods of the instance are
-    # published as XML-RPC methods (in this case, just 'mul').
-    class MyFuncs:
-        def mul(self, x, y):
-            return x * y
-
-    server.register_instance(MyFuncs())
-
-    # Run the server's main loop
+    server.register_function(do_something)
     server.serve_forever()
