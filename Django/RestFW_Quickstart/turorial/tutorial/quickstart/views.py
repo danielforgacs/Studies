454e650b99e68fb1ee5838da4392a08017171a5e
df@ <@>
2021-03-03 08:18:04 +0000

    started model view sets

--- Django/RestFW_Quickstart/turorial/tutorial/quickstart/views.py
+++ Django/RestFW_Quickstart/turorial/tutorial/quickstart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import django.contrib.auth.models as authmodels
+# from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer
+from tutorial.quickstart.serializers import GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = authmodels.User.objects.all().order_by('-date-joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = authmodels.Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
