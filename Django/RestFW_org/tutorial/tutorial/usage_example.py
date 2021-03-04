fd6e0ceaacc830e3f4956bf8752d0326497bbacc
df@ <@>
2021-03-04 22:12:58 +0000

    added renderer exampe to usage example

--- Django/RestFW_org/tutorial/tutorial/usage_example.py
+++ Django/RestFW_org/tutorial/tutorial/usage_example.py
@@ -5,6 +5,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tutorial.settings'
 import django
 django.setup()
 
+from rest_framework import renderers
+from rest_framework import parsers
 import snippets.models as mod
 import snippets.serializers as ser
 
@@ -20,3 +22,7 @@ serializer1 = ser.SnippetSerializer(instance=snippet1)
 
 print(serializer1.data)
 print(serializer1.data['code'])
+
+content = renderers.JSONRenderer().render(serializer1.data)
+
+print(content)
