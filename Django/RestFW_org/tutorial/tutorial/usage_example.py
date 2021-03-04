de4144aca6c86fe609994d22bfde065e12aa34cd
df@ <@>
2021-03-04 22:06:51 +0000

    started usage example

---
+++ Django/RestFW_org/tutorial/tutorial/usage_example.py
@@ -0,0 +1,22 @@
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tutorial.settings'
+
+import django
+django.setup()
+
+import snippets.models as mod
+import snippets.serializers as ser
+
+
+snippet1 = mod.Snippet()
+snippet1.code = """
+de fukkin():
+    print(';laksdjf')
+"""
+snippet1.save()
+
+serializer1 = ser.SnippetSerializer(instance=snippet1)
+
+print(serializer1.data)
+print(serializer1.data['code'])
