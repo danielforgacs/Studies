018555ec1022f05141ecb5b414097c0a74266c38
df@ <@>
2021-03-06 09:50:08 +0000

    wrote snippet list view for model serializer

--- Django/RestFW_org/tutorial/tutorial/snippets/views.py
+++ Django/RestFW_org/tutorial/tutorial/snippets/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+import snippets
+import django.http as http
+import django.views.decorators as decorators
+import rest_framework.parsers as parsers
+import snippets.models as models
+import snippets.serializers as serializers
 
-# Create your views here.
+
+
+
+@decorators.csrf_exempt
+def snippet_list(request):
+    if request.method == 'GET':
+        snippets = models.Snippet.objects.all()
+        serializer = serializers.SnippetModelSerializer(data=snippets, many=True)
+        response = http.JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = parsers.JSONParser(request)
+        serializer = serializers.SnippetModelSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return http.JsonResponse(serializer.data, status=201)
+
+        return http.JsonResponse(serializer.errors, status=400)
