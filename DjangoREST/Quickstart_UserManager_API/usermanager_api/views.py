7411f43c9becc7ae55d43fc62a07864dc0cb7a87
df@ <@>
2021-03-07 15:15:06 +0000

    cretated user and group rest veiw sets

--- DjangoREST/Quickstart_UserManager_API/usermanager_api/views.py
+++ DjangoREST/Quickstart_UserManager_API/usermanager_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import django.contrib.auth.models as authmodels
+import rest_framework.viewsets as viewsets
+import rest_framework.permissions as permissions
+import usermanager_api.serialisers as serialisers
 
-# Create your views here.
+
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = authmodels.User.objects.all()
+    serializer_class = serialisers.UserSerialiser
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = authmodels.User.groups.all()
+    serializer_class = serialisers.GroudSerialiser
+    permission_classes = [permissions.IsAuthenticated]
