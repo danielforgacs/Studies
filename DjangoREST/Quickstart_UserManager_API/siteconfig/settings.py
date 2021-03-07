a889e6f40d65f14d680c829c16e3ec107ba71f46
df@ <@>
2021-03-07 14:39:21 +0000

    installed user manager & rest framework apps

--- DjangoREST/Quickstart_UserManager_API/siteconfig/settings.py
+++ DjangoREST/Quickstart_UserManager_API/siteconfig/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usermanager_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
