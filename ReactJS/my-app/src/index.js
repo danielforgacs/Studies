380568db229fd6e924303b78386d8287ad5dd010
df@ <@>
2021-04-13 00:44:53 +0100

    passing values/variables/arguments

--- ReactJS/my-app/src/index.js
+++ ReactJS/my-app/src/index.js
@@ -9,6 +9,7 @@ class Square extends React.Component {
       return (
         <button className="square">
           {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
@@ -16,7 +17,7 @@ class Square extends React.Component {
 
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i}/>;
     }
 
     render() {
