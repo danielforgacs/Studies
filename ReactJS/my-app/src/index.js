a5d7920279dff6ed153ba0cc8f016085ae254fc2
df@ <@>
2021-04-13 23:52:42 +0100

    reformat

--- ReactJS/my-app/src/index.js
+++ ReactJS/my-app/src/index.js
@@ -14,7 +14,10 @@ class Square extends React.Component {
 
     render () {
         return (
-            <button className="square" onClick={() => this.setState({value: "x"})}>
+            <button
+                className="square"
+                onClick={() => this.setState({value: "x"})}
+            >
                 {/* TODO */}
                 {/* {this.props.value} */}
                 {this.state.value}
