b9a9807f90569a8091f83c7c259604455f5236ac
df@ <@>
2021-04-13 23:44:15 +0100

    refactored to arrow syntax

--- ReactJS/my-app/src/index.js
+++ ReactJS/my-app/src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 class Square extends React.Component {
     render () {
         return (
-            <button className="square" onClick={function () {alert("click")}}>
+            <button className="square" onClick={() => alert("click")}>
                 {/* TODO */}
                 {this.props.value}
             </button>
