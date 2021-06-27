fb47d7410e45a4ec610d04b8ffa12f4e8790da44
df@ <@>
2021-06-27 23:53:27 +0100

    updated docs example

--- AsyncPython/as03.py
+++ AsyncPython/as03.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 
 async def say_after(delay, what):
+    print('.say_after')
     await asyncio.sleep(delay)
     print(what)
 
@@ -13,12 +14,20 @@ async def main():
     task2 = asyncio.create_task(
         say_after(5, 'world'))
 
+    task3 = asyncio.create_task(
+        say_after(5, 'world'))
+
+    task4 = asyncio.create_task(
+        say_after(5, 'world'))
+
     print(f"started at {time.strftime('%X')}")
 
     # Wait until both tasks are completed (should take
     # around 2 seconds.)
     await task1
     await task2
+    await task3
+    await task4
 
     print(f"finished at {time.strftime('%X')}")
 
