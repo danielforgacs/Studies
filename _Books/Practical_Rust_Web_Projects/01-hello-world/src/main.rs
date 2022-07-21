57263b11bc6e29e00f5957ae14a11ef95085892d
df@ <@>
2022-07-21 13:12:50 +0100

    wrote hellow world web app

--- _Books/Practical_Rust_Web_Projects/01-hello-world/src/main.rs
+++ _Books/Practical_Rust_Web_Projects/01-hello-world/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder, HttpResponse};
+use actix_web::web;
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("http://localhost:8080");
+    HttpServer::new(|| {
+        App::new()
+        .route("/hello", web::get().to(hello))
+    })
+    .bind("localhost:8080")?
+    .run()
+    .await
 }
