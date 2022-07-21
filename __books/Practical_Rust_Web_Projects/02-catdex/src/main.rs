3a1e39af81fcb50f5cd0572f0f718f7c5280e067
df@ <@>
2022-07-21 23:10:22 +0100

    serving static html named-file ok

--- __books/Practical_Rust_Web_Projects/02-catdex/src/main.rs
+++ __books/Practical_Rust_Web_Projects/02-catdex/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::{NamedFile};
+use actix_web::{App, HttpServer, Result};
+use actix_web::web;
+
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("static/index.html")?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("http://localhost:8090");
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(index))
+    })
+    .bind("localhost:8090")?
+    .run()
+    .await
 }
