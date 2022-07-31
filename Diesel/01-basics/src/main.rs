d023f72e5e3ea65af1a7c417a4fb62eb35dd30bf
df@ <@>
2022-07-31 22:01:00 +0100

    works ;

--- Diesel/01-basics/src/main.rs
+++ Diesel/01-basics/src/main.rs
@@ -1,10 +1,19 @@
 #[macro_use]
 extern crate diesel;
 
+mod schema;
+
 use dotenv;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use std::env;
+// use schema;
+
+#[derive(Queryable, Debug)]
+struct Stuff {
+    id: i32,
+    cool: String,
+}
 
 pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL")
@@ -13,7 +22,15 @@ pub fn establish_connection() -> PgConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     dotenv::dotenv().ok();
+    use crate::schema::stuff::dsl::*;
+
     let db_conn = establish_connection();
+    let results = stuff
+        .limit(3)
+        .load::<Stuff>(&db_conn)
+        .map_err(|e| dbg!("error: {}", e));
+    dbg!(results);
+    Ok(())
 }
