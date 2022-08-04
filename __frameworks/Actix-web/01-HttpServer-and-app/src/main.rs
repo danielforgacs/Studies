f635197df9b0c22e677671878b6fbc03d128f658
df@ <@>
2022-08-04 19:45:36 +0100

    http server + app / no routes

--- __frameworks/Actix-web/01-HttpServer-and-app/src/main.rs
+++ __frameworks/Actix-web/01-HttpServer-and-app/src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(
+        move || {
+            App::new()
+        }
+    )
+        .bind("127.0.0.1:8888")?
+        .run()
+        .await
+}
+
