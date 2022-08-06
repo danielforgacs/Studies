7b943e4d88aae66af4919b25b24ec0cec654c8b7
df@ <@>
2022-08-06 21:50:28 +0100

    pg connection

--- __books/Practical_Rust_Web_Projects/03-catdex-api/src/main.rs
+++ __books/Practical_Rust_Web_Projects/03-catdex-api/src/main.rs
@@ -1,6 +1,23 @@
 use actix_web::{App, HttpServer};
 use actix_files::{Files};
-use diesel;
+
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
