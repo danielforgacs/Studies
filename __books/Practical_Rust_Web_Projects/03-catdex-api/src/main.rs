28745e01683b5358de867a04314b6789581f3147
df@ <@>
2022-08-06 21:20:59 +0100

    started project

--- __books/Practical_Rust_Web_Projects/03-catdex-api/src/main.rs
+++ __books/Practical_Rust_Web_Projects/03-catdex-api/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use actix_web::{App, HttpServer};
+use actix_files::{Files};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(
+        move || {
+            App::new()
+                .service(
+                    Files::new("/", "static")
+                )
+        }
+    )
+        .bind("127.0.0.1:8090")?
+        .run()
+        .await
+}
