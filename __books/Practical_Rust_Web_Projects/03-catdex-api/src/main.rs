a27d82448733b4dc6ab5958f34927dcb810533d6
df@ <@>
2022-08-07 10:42:46 +0100

    Revert "__wip__; pool"
    This reverts commit 27d235af2480631d21cde5c3ff13c57410c2f37d.

--- __books/Practical_Rust_Web_Projects/03-catdex-api/src/main.rs
+++ __books/Practical_Rust_Web_Projects/03-catdex-api/src/main.rs
@@ -10,10 +10,8 @@ use serde::{Serialize};
 use diesel::prelude::*;
 use crate::schema::cat::dsl::*;
 use diesel::pg::PgConnection;
-use diesel::r2d2::ConnectionManager;
 use dotenv::dotenv;
 use std::env;
-use r2d2;
 
 #[derive(Queryable, Serialize, Debug)]
 struct Cats {
@@ -26,13 +24,11 @@ async fn alive() -> impl Responder {
     "alive"
 }
 
-async fn api_cats(pool: web::Data<r2d2::Pool<ConnectionManager::<diesel::PgConnection>>>) -> HttpResponse {
-    let mut conn = pool.get().expect("can't get db pool in 'api_cats()'");
-    let query = web::block(move || {
-        cat.load::<Cats>(&mut conn)
-    })
-    .await
-    .expect("something's wrong");
+async fn api_cats() -> HttpResponse {
+    let mut conn = establish_connection();
+    let query = cat
+        .load::<Cats>(&mut conn)
+        .expect("Can't query cats.");
     HttpResponse::Ok().json(query)
 }
 
@@ -51,21 +47,9 @@ pub fn establish_connection() -> PgConnection {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-
-    let manager = ConnectionManager::<diesel::PgConnection>::new(&database_url);
-    let pool = r2d2::Pool::builder()
-        .max_size(25)
-        .build(manager)
-        .expect("Can't create db connection pool");
-
     HttpServer::new(
         move || {
             App::new()
-                .app_data(pool.clone())
                 .service(
                     Files::new("/static", "static")
                 )
