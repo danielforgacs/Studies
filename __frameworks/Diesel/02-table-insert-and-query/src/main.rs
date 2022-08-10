9d6123cd3d31f444dc664d67b14b08364ad50d5b
df@ <@>
2022-08-10 19:30:45 +0100

    pgconn

--- __frameworks/Diesel/02-table-insert-and-query/src/main.rs
+++ __frameworks/Diesel/02-table-insert-and-query/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use diesel::prelude::*;
+use diesel::PgConnection;
+use dotenv;
+
+fn establish_db_connection() -> Result<PgConnection, Box<dyn std::error::Error>> {
+    let db_url = std::env::var("DATABASE_URL")?;
+    Ok(PgConnection::establish(&db_url)?)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    let db_conn = establish_db_connection()?;
+    Ok(())
 }
