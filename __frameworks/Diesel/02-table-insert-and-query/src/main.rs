c9e162328935801be8dfdb9805529e7e1310f3ef
df@ <@>
2022-08-10 19:43:28 +0100

    ok

--- __frameworks/Diesel/02-table-insert-and-query/src/main.rs
+++ __frameworks/Diesel/02-table-insert-and-query/src/main.rs
@@ -1,6 +1,20 @@
+#[macro_use]
+extern crate diesel;
+mod schema;
+
 use diesel::prelude::*;
-use diesel::PgConnection;
+use diesel::{PgConnection, Queryable};
+use schema::items_table::dsl::*;
 use dotenv;
+
+mod model {
+    use super::*;
+    #[derive(Queryable)]
+    struct StructItem {
+        id: i32,
+        item_name_field: String,
+    }
+}
 
 fn establish_db_connection() -> Result<PgConnection, Box<dyn std::error::Error>> {
     let db_url = std::env::var("DATABASE_URL")?;
@@ -10,5 +24,6 @@ fn establish_db_connection() -> Result<PgConnection, Box<dyn std::error::Error>>
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
     let db_conn = establish_db_connection()?;
+    // let all_items =
     Ok(())
 }
