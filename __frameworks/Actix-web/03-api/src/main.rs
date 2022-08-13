8e8f693b910a4ff95ad498c0867e0e4068b322c9
df@ <@>
2022-08-14 00:36:38 +0100

    set up server and app

--- __frameworks/Actix-web/03-api/src/main.rs
+++ __frameworks/Actix-web/03-api/src/main.rs
@@ -1,2 +1,16 @@
-fn main() {
+use actix_web::{HttpServer, App};
+
+const URL: &str = "127.0.0.1:8888";
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    println!("http://{}", URL);
+    HttpServer::new(
+        move || {
+            App::new()
+        }
+    )
+        .bind(URL)?
+        .run()
+        .await
 }
