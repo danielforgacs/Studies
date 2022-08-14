81ffb53be9e3815e1491b41fe362e089374ad807
df@ <@>
2022-08-14 01:12:31 +0100

    log

--- __frameworks/Actix-web/03-api/src/main.rs
+++ __frameworks/Actix-web/03-api/src/main.rs
@@ -3,7 +3,10 @@ use actix_web::{
         HttpServer,
         App,
         HttpResponse,
+        middleware::Logger,
     };
+use env_logger;
+use dotenv;
 
 const URL: &str = "127.0.0.1:8888";
 
@@ -13,10 +16,13 @@ async fn alive() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+    dotenv::dotenv().ok();
     println!("http://{}", URL);
+    env_logger::init();
     HttpServer::new(
         move || {
             App::new()
+                .wrap(Logger::new("%a %{User-Agent}i"))
                 .route("/alive", web::get().to(alive))
         }
     )
