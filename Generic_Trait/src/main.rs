4b40e1945fa5afe08c40646b4f2d0485320d91c7
df@ <@>
2022-08-29 23:08:50 +0100

    ok

--- Generic_Trait/src/main.rs
+++ Generic_Trait/src/main.rs
@@ -1,18 +1,9 @@
 #[derive(Debug)]
 struct A;
 
-#[derive(Debug)]
-struct B;
-
 impl A {
     fn print_self(&self) {
         println!("impl print a: {:?}", self);
-    }
-}
-
-impl B {
-    fn print_self(&self) {
-        println!("impl print B: {:?}", self);
     }
 }
 
@@ -22,23 +13,10 @@ trait GenericPrint {
         Self: std::fmt::Debug
     {
         println!("generic print: {:?}, cloned: {:?}", self, self.clone());
-        self.call_print_self();
-    }
-
-    fn call_print_self(&self);
-}
-
-impl GenericPrint for A {
-    fn call_print_self(&self) {
-        self.print_self();
     }
 }
 
-impl GenericPrint for B {
-    fn call_print_self(&self) {
-        self.print_self();
-    }
-}
+impl GenericPrint for A {}
 
 fn generic_func<T>(item: &T) -> [&T; 2] {
     [&item, &item]
@@ -46,17 +24,8 @@ fn generic_func<T>(item: &T) -> [&T; 2] {
 
 fn main() {
     let a = A {};
-    let b = B {};
-
     a.print_self();
-    b.print_self();
-
     a.genp();
-    b.genp();
-
     println!("items: {:?}", generic_func(&a));
-    println!("items: {:?}", generic_func(&b));
-
     println!("items: {:?}", generic_func::<A>(&a));
-    println!("items: {:?}", generic_func::<B>(&b));
 }
