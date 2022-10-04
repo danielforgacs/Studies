a0d83961acf414798d1d39abeee40777750df975
df@ <@>
2022-10-05 00:02:54 +0100

    added example

--- error_handling_capturing_multiple_errors_in_a_func/src/main.rs
+++ error_handling_capturing_multiple_errors_in_a_func/src/main.rs
@@ -1,22 +1,22 @@
 #[derive(Debug, Clone)]
 enum FileOpenError{
-    Error_A(String),
+    ErrorA(String),
 }
 
 impl From<std::io::Error> for FileOpenError {
     fn from(err: std::io::Error) -> Self {
-        Self::Error_A("alksdfhj".to_string())
+        Self::ErrorA(format!("[ERROR STUFF::{}::{}]", err.kind(), err.to_string()))
     }
 }
 
 fn main() {
-    let db_file = match open_file("db_file_name.txt") {
-        Err(FileOpenError::Error_A(msg)) => println!("{}", msg),
+    let _db_file = match open_file("db_file_name.txt") {
+        Err(FileOpenError::ErrorA(msg)) => println!("{}", msg),
         Ok(_) => (),
     };
 }
 
 fn open_file(db_file_name: &str) -> Result<std::fs::File, FileOpenError> {
     std::fs::File::open(db_file_name)?;
-    Err(FileOpenError::Error_A("EOF".to_string()))
+    Err(FileOpenError::ErrorA("EOF".to_string()))
 }
