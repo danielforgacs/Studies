110dab2f7b395312672e57bb3eed4a638aa77fe5
df@ <@>
2022-10-04 23:58:40 +0100

    not working yet

--- error_handling_capturing_multiple_errors_in_a_func/src/main.rs
+++ error_handling_capturing_multiple_errors_in_a_func/src/main.rs
@@ -1,12 +1,22 @@
 #[derive(Debug, Clone)]
 enum FileOpenError{
-    Error_A,
+    Error_A(String),
+}
+
+impl From<std::io::Error> for FileOpenError {
+    fn from(err: std::io::Error) -> Self {
+        Self::Error_A("alksdfhj".to_string())
+    }
 }
 
 fn main() {
-    let db_file = open_file("db_file_name.txt").unwrap();
+    let db_file = match open_file("db_file_name.txt") {
+        Err(FileOpenError::Error_A(msg)) => println!("{}", msg),
+        Ok(_) => (),
+    };
 }
 
 fn open_file(db_file_name: &str) -> Result<std::fs::File, FileOpenError> {
-    Err(FileOpenError::Error_A)
+    std::fs::File::open(db_file_name)?;
+    Err(FileOpenError::Error_A("EOF".to_string()))
 }
