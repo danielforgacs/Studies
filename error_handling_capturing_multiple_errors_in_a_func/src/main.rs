2c067d1828e10cb3bff79ed23df30cb1d54d3c4e
df@ <@>
2022-10-05 00:20:24 +0100

    added example

--- error_handling_capturing_multiple_errors_in_a_func/src/main.rs
+++ error_handling_capturing_multiple_errors_in_a_func/src/main.rs
@@ -1,3 +1,12 @@
+/*
+
+basic example:
+https://stackoverflow.com/questions/71812362/rust-error-handling-capturing-multiple-errors
+
+*/
+
+use std::io::prelude::*;
+
 #[derive(Debug, Clone)]
 enum FileOpenError{
     ErrorA(String),
@@ -14,9 +23,23 @@ fn main() {
         Err(FileOpenError::ErrorA(msg)) => println!("{}", msg),
         Ok(_) => (),
     };
+    match another_file_error() {
+        Err(FileOpenError::ErrorA(msg)) => println!("{}", msg),
+        Ok(_) => (),
+    };
 }
 
 fn open_file(db_file_name: &str) -> Result<std::fs::File, FileOpenError> {
     std::fs::File::open(db_file_name)?;
     Err(FileOpenError::ErrorA("EOF".to_string()))
+}
+
+fn another_file_error() -> Result<(), FileOpenError> {
+    // Set the permissions to 0 before running this,
+    // so the error show up!
+    // $ chmod 0 __delete.txt
+    let mut file = std::fs::File::create("__delete.txr")?;
+    let buf = String::new();
+    file.write_all(&buf.as_bytes()).unwrap();
+    Ok(())
 }
