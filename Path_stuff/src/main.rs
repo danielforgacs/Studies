ac1fb6199148b1daf0b6c76deb84efd46cfe65e4
df@ <@>
2022-10-05 15:21:18 +0100

    ok

--- Path_stuff/src/main.rs
+++ Path_stuff/src/main.rs
@@ -34,4 +34,40 @@ fn main() {
         let abs_path = file_path.canonicalize();
         dbg!(&abs_path);
     }
+
+//////////////////////////////////
+
+    {
+        let file_name = "Cargo.toml".to_string();
+        let file_path = Path::new(&file_name);
+        dbg!(&file_path);
+        let abs_path = file_path.canonicalize();
+        dbg!(&abs_path);
+    }
+
+    {
+        let file_name = "Cargo.toml".to_string();
+        let mut file_path = PathBuf::new();
+        file_path.push(file_name);
+        dbg!(&file_path);
+        let abs_path = file_path.canonicalize();
+        dbg!(&abs_path);
+    }
+
+    {
+        let file_name = "./Cargo.toml".to_string();
+        let file_path = Path::new(&file_name);
+        dbg!(&file_path);
+        let abs_path = file_path.canonicalize();
+        dbg!(&abs_path);
+    }
+
+    {
+        let file_name = "./Cargo.toml".to_string();
+        let mut file_path = PathBuf::new();
+        file_path.push(file_name);
+        dbg!(&file_path);
+        let abs_path = file_path.canonicalize();
+        dbg!(&abs_path);
+    }
 }
