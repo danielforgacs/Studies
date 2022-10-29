7103df51b830926bab3a2d04984e54d892f6906e
df@ <@>
2022-10-30 00:53:54 +0100

    ok

--- node_visitor/src/main.rs
+++ node_visitor/src/main.rs
@@ -9,13 +9,20 @@ struct Node {
 }
 
 fn main() {
-    let node0 = Node { left: NodeLink::Text("A".to_string()), right: Option::None };
+    let node0 = Node {
+        left: NodeLink::Text("A".to_string()),
+        right: Option::Some(NodeLink::Text("B".to_string()))
+    };
     node_visitor(node0);
 }
 
 fn node_visitor(node: Node) {
     match node.left {
         NodeLink::Text(text) => println!("{}", text),
+        _ => (),
+    }
+    match node.right {
+        Option::Some(NodeLink::Text(text)) => println!("{}", text),
         _ => (),
     }
 }
