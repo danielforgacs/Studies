771b64f78f1de4977cf8877c0ffec6e155bdb111
df@ <@>
2022-10-30 13:40:59 +0000

    ok

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,3 +1,45 @@
+enum GrammarItem {
+    Product,
+    Sum,
+    Number(u64),
+    Paren,
+}
+
+enum LexItem {
+    Paren(char),
+    Op(char),
+    Num(u64),
+}
+
+struct ParseNode {
+    children: Vec<ParseNode>,
+    entry: GrammarItem,
+}
+
+impl ParseNode {
+    fn new() -> Self {
+        Self {
+            children: Vec::new(),
+            entry: GrammarItem::Paren,
+        }
+    }
+}
+
+fn lex(input: String) -> Result<Vec<LexItem>, String> {
+    let mut result = Vec::new();
+    let mut it = input.chars().peekable();
+    while let Some(&c) = it.peek() {
+        match c {
+            '0'..='9' => {},
+            '+' | '-' => {},
+            '(' | ')' | '[' | ']' | '{' | '}' => {},
+            ' ' => {},
+            _ => {},
+        }
+    };
+    Ok(result)
+}
+
 fn main() {
     println!("Hello, world!");
 }
