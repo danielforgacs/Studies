f38a1eab490ceffdd374801115ba4064f355a5c7
df@ <@>
2023-01-02 21:57:07 +0000

    ok

--- Algorithms/01_merge_sort/src/main.rs
+++ Algorithms/01_merge_sort/src/main.rs
@@ -2,24 +2,10 @@ fn main() {
 }
 
 fn merge_sort(a: Vec<u8>) -> Vec<u8> {
-    if a.len() == 1 {
+    if a.len() <= 2 {
         return a;
-    } else if a.len() == 2 {
-        if a[0] > a[1] {
-            return vec![a[1], a[0]];
-        }
-        return a;
-    } else {
-        let mut result: Vec<u8> = Vec::new();
-        let half: usize = (a.len() / 2) as usize;
-        let a1: Vec<u8> = a[0..half].to_vec();
-        let a2: Vec<u8> = a[half + 1..a.len() - 1].to_vec();
-
-        result.extend(merge_sort(a1));
-        result.extend(merge_sort(a2));
-
-        return result;
     }
+    vec![]
 }
 
 #[cfg(test)]
@@ -27,14 +13,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_merge_sort_n2() {
-        let mut array = vec![2, 1];
-        assert_eq!(merge_sort(array), vec![1, 2]);
+    fn test_merge_sort_n0() {
+        let array = vec![];
+        assert_eq!(merge_sort(array), vec![]);
     }
 
     #[test]
-    fn test_merge_sort_n3() {
-        let mut array = vec![3, 2, 1];
-        assert_eq!(merge_sort(array), vec![1, 2, 3]);
+    fn test_merge_sort_n1() {
+        let array = vec![1];
+        assert_eq!(merge_sort(array), vec![1]);
     }
 }
