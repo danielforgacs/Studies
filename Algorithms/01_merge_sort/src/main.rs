8c2db7a8874d44127f2df13d53119c78a6054270
df@ <@>
2023-01-02 17:43:46 +0000

    ..

--- Algorithms/01_merge_sort/src/main.rs
+++ Algorithms/01_merge_sort/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+}
+
+fn merge_sort(a: &mut Vec<u8>) {
+    *a = vec![2, 1];
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_sort() {
+        let mut array = vec![2, 1];
+        merge_sort(&mut array);
+        assert_eq!(array, vec![1, 2]);
+    }
 }
