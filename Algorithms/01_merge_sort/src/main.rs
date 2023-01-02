3378943f82ae368c2bcb33d66302c9a037d8d7fc
df@ <@>
2023-01-02 22:07:41 +0000

    wip

--- Algorithms/01_merge_sort/src/main.rs
+++ Algorithms/01_merge_sort/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
+    merge_sort(vec![1, 2]);
 }
 
 fn merge_sort(a: Vec<u8>) -> Vec<u8> {
+    println!("a: {:?}", a);
     if a.len() <= 2 {
         return a;
+    } else {
+
     }
     vec![]
 }
@@ -22,5 +26,11 @@ mod tests {
     fn test_merge_sort_n1() {
         let array = vec![1];
         assert_eq!(merge_sort(array), vec![1]);
+    }
+
+    #[test]
+    fn test_merge_sort_n2() {
+        let array = vec![2, 1];
+        assert_eq!(merge_sort(array), vec![1, 2]);
     }
 }
