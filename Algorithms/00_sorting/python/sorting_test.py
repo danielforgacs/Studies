03ebe6fd8f8ee061fd4418723b2cbe9dfa11761f
df@ <@>
2023-01-07 22:25:41 +0000

    merge sort

--- Algorithms/00_sorting/python/sorting_test.py
+++ Algorithms/00_sorting/python/sorting_test.py
@@ -13,6 +13,42 @@ def test_insertion_sort():
     assert a == sorted(a), a
 
 
+def merge_sort(arr):
+    if len(arr) < 2:
+        return
+    mid = len(arr)//2
+    L = arr[:mid]
+    R = arr[mid:]
+    print(arr, '->', L, R)
+    merge_sort(L)
+    merge_sort(R)
+    i = j = k = 0
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
 
-# if __name__ == '__main__':
-#     print(a)
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def test_merge_sort():
+    a = [555, 444, 333, 222, 111]
+    merge_sort(a)
+    assert a == sorted(a)
+
+
+if __name__ == '__main__':
+    a = [5, 4, 3, 2, 1]
+    merge_sort(a)
+    print('------------------------')
+    print(a)
