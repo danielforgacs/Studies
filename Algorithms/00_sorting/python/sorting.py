0c28ba751744b453981676b9daa33c5ce1d15a37
df@ <@>
2023-01-07 16:17:27 +0000

    ok

--- Algorithms/00_sorting/python/sorting.py
+++ Algorithms/00_sorting/python/sorting.py
@@ -1,13 +1,14 @@
 def insertion_sort(a):
-    for j in range(1, len(a)):
-        key = a[j]
-        i = j - 1
-        while (i > 0) and (a[i] > key):
+    for k in range(1, len(a)):
+        key = a[k]
+        i = k - 1
+        while (i >= 0) and (a[i] > key):
             a[i+1] = a[i]
             i -= 1
         a[i+1] = key
 
 if __name__ == '__main__':
-    a = [300, 200, 100]
+    a = [500, 400, 300, 200, 100]
     insertion_sort(a)
+    assert a == sorted(a), a
     print(a)
