569ff3f0d109463b419357a1f5af09f89ff475cc
df@ <@>
2023-01-07 02:57:34 +0000

    wip

--- Algorithms/00_sorting/src/main.rs
+++ Algorithms/00_sorting/src/main.rs
@@ -1,27 +1,29 @@
 fn main() {
-    let mut a = vec![9, 1, 8, 2, 7, 3];
+    // let mut a = vec![9, 1, 8, 2, 7, 3];
+    let mut a = vec![300, 200, 100];
     insertion_sort(&mut a);
+    println!("a: {:?}", a);
 }
 
-fn insertion_sort(a: &mut Vec<u8>) {
+fn insertion_sort(a: &mut Vec<i32>) {
+    // for j in 1..a.len() {
+    //     let mut i = j;
+    //     while i > 0 && a[i-1] > a[i] {
+    //         let temp = a[i-1];
+    //         a[i-1] = a[i];
+    //         a[i] = temp;
+    //         i -= 1;
+    //     }
+    // }
     for j in 1..a.len() {
-        let mut i = j;
-        while i > 0 && a[i-1] > a[i] {
-            let temp = a[i-1];
-            a[i-1] = a[i];
-            a[i] = temp;
+        let key = a[j];
+        let mut i = j - 1;
+        while i > 0 && a[i] > key {
+            a[i+1] = a[i];
             i -= 1;
         }
+        a[i+1] = key;
     }
-    // for j in 1..a.len() {
-    //     let key = a[j];
-    //     let mut i = j - 1;
-    //     while i > 0 && a[i] > key {
-    //         a[i+1] = a[i];
-    //         i = i - 1;
-    //     }
-    //     a[i+1] = key;
-    // }
 }
 
 #[cfg(test)]
