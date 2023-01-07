6e51abbae8d021816b8c980436d892328e7af8a5
df@ <@>
2023-01-07 17:02:22 +0000

    added insertion sort without casting

--- Algorithms/00_sorting/src/main.rs
+++ Algorithms/00_sorting/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    // let mut a = vec![9, 1, 8, 2, 7, 3];
-    let mut a = vec![300, 200, 100];
-    insertion_sort(&mut a);
+    let mut a = vec![555, 444, 333, 222, 111];
+    insertion_sort_no_cast(&mut a);
     println!("a: {:?}", a);
 }
 
@@ -44,14 +43,38 @@ fn insertion_sort(a: &mut Vec<i32>) {
     }
 }
 
+fn insertion_sort_no_cast(a: &mut Vec<i32>) {
+    /*
+    In this version the index does not go
+    below zero while working, so no type cast is needed.
+    But this way the indexes are shifted with one higher.
+    */
+    for mut k in 2..=a.len() {
+        let key = a[k - 1];
+        let mut i = k - 1;
+        while i > 0 && a[i - 1] > key {
+            a[i] = a[i - 1];
+            i -= 1;
+        }
+        a[i] = key;
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn test_insertion_sort() {
-        let mut a = vec![9, 1, 8, 2, 7, 3];
+        let mut a = vec![7, 6, 5, 4, 3, 2, 1];
         insertion_sort(&mut a);
-        assert_eq!(a, vec![1, 2, 3, 7, 8, 9]);
+        assert_eq!(a, vec![1, 2, 3, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn test_insertion_sort_no_cast() {
+        let mut a = vec![7, 6, 5, 4, 3, 2, 1];
+        insertion_sort_no_cast(&mut a);
+        assert_eq!(a, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 }
