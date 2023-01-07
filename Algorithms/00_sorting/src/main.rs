f51731cffc155cea801dacf2ec33ea9952b682f6
df@ <@>
2023-01-07 01:18:09 +0000

    started sorting algs

---
+++ Algorithms/00_sorting/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_insertion_sort() {
+        let a = vec![9, 1, 8, 2, 8, 3];
+    }
+}
