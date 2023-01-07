9fbcefd357aa1fc07746536d224ff7fa511403a8
df@ <@>
2023-01-07 02:37:42 +0000

    insertion sort ok

--- Algorithms/00_sorting/src/main.rs
+++ Algorithms/00_sorting/src/main.rs
@@ -1,5 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a = vec![9, 1, 8, 2, 7, 3];
+    insertion_sort(&mut a);
+}
+
+fn insertion_sort(a: &mut Vec<u8>) {
+    for j in 1..a.len() {
+        let mut i = j;
+        while i > 0 && a[i-1] > a[i] {
+            let temp = a[i-1];
+            a[i-1] = a[i];
+            a[i] = temp;
+            i -= 1;
+        }
+    }
+    // for j in 1..a.len() {
+    //     let key = a[j];
+    //     let mut i = j - 1;
+    //     while i > 0 && a[i] > key {
+    //         a[i+1] = a[i];
+    //         i = i - 1;
+    //     }
+    //     a[i+1] = key;
+    // }
 }
 
 #[cfg(test)]
@@ -8,6 +30,8 @@ mod test {
 
     #[test]
     fn test_insertion_sort() {
-        let a = vec![9, 1, 8, 2, 8, 3];
+        let mut a = vec![9, 1, 8, 2, 7, 3];
+        insertion_sort(&mut a);
+        assert_eq!(a, vec![1, 2, 3, 7, 8, 9]);
     }
 }
