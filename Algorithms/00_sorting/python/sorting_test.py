ca5cb1eb2b6b2cc39b72a469a404ef2abf00a3ab
df@ <@>
2023-01-08 11:08:02 +0000

    clean up

--- Algorithms/00_sorting/python/sorting_test.py
+++ Algorithms/00_sorting/python/sorting_test.py
@@ -7,39 +7,6 @@ def insertion_sort(a):
             i -= 1
         a[i+1] = key
 
-def test_insertion_sort():
-    a = [500, 400, 300, 200, 100]
-    insertion_sort(a)
-    assert a == sorted(a), a
-
-
-# def merge_sort(arr):
-#     if len(arr) < 2:
-#         return
-#     mid = len(arr)//2
-#     L = arr[:mid]
-#     R = arr[mid:]
-#     print(arr, '->', L, R)
-#     merge_sort(L)
-#     merge_sort(R)
-#     i = j = k = 0
-#     while i < len(L) and j < len(R):
-#         if L[i] <= R[j]:
-#             arr[k] = L[i]
-#             i += 1
-#         else:
-#             arr[k] = R[j]
-#             j += 1
-#         k += 1
-#     while i < len(L):
-#         arr[k] = L[i]
-#         i += 1
-#         k += 1
-
-#     while j < len(R):
-#         arr[k] = R[j]
-#         j += 1
-#         k += 1
 
 def merge_sort(a):
     if len(a) < 2:
@@ -47,10 +14,8 @@ def merge_sort(a):
     mid = len(a) // 2
     l = a[:mid]
     r = a[mid:]
-    # print('array:', a, '->', 'l:', l, 'r:', r)
     merge_sort(l)
     merge_sort(r)
-    print('after l:', l, 'r:', r)
     i = j = 0
     for k, _ in enumerate(a):
         if i == len(l):
@@ -65,6 +30,13 @@ def merge_sort(a):
         else:
             a[k] = r[j]
             j += 1
+
+
+def test_insertion_sort():
+    a = [500, 400, 300, 200, 100]
+    insertion_sort(a)
+    assert a == sorted(a), a
+
 
 def test_merge_sort():
     a = [555, 444, 333, 222, 111]
