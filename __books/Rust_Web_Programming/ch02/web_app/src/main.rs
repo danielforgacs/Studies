710931b98097969deda43d02ea0c00ec36687415
df@ <@>
2023-03-21 19:48:34 +0000

    generating random number

--- __books/Rust_Web_Programming/ch02/web_app/src/main.rs
+++ __books/Rust_Web_Programming/ch02/web_app/src/main.rs
@@ -1,3 +1,12 @@
+use rand::prelude::*;
+
+fn generate_float(generator: &mut ThreadRng) -> f64 {
+    let placeholder: f64 = generator.gen();
+    return placeholder * 10.0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let random_number = generate_float(&mut rng);
+    println!("generated random num: {}", random_number);
 }
