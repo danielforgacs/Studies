a56ba027a27731752098ada6d8f5f67987373115
df@ <@>
2023-03-25 18:41:48 +0000

    ok

--- __books/Rust_Web_Programming/ch02/todo_app/src/main.rs
+++ __books/Rust_Web_Programming/ch02/todo_app/src/main.rs
@@ -3,5 +3,11 @@ mod to_do;
 use to_do::structs::{done::Done, pending::Pending};
 
 fn main() {
-    // let done =
+    let done = Done::new("shopping todo");
+    println!("{}", done.super_struct.title);
+    println!("{}", done.super_struct.status);
+
+    let pending = Pending::new("pending todo");
+    println!("{}", pending.super_struct.title);
+    println!("{}", pending.super_struct.status);
 }
