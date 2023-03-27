e5515383a35fed9fd7087d24222d4006ce5d428c
df@ <@>
2023-03-27 20:17:48 +0100

    implemented crud traits for pending.

--- __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/pending.rs
+++ __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/pending.rs
@@ -1,4 +1,10 @@
 use super::base::*;
+use super::traits::{
+    create::Create,
+    get::Get,
+    edit::Edit,
+    delete::Delete,
+};
 
 pub struct Pending {
     pub super_struct: Base,
@@ -10,3 +16,8 @@ impl Pending {
         Self { super_struct }
     }
 }
+
+impl Create for Pending {}
+impl Get for Pending {}
+impl Edit for Pending {}
+impl Delete for Pending {}
