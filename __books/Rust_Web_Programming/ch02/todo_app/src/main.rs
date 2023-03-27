26d418a3eb7ac6497eb3b1585fab17eebac4bc3e
df@ <@>
2023-03-27 23:47:51 +0100

    ok.

--- __books/Rust_Web_Programming/ch02/todo_app/src/main.rs
+++ __books/Rust_Web_Programming/ch02/todo_app/src/main.rs
@@ -3,12 +3,13 @@ mod to_do;
 use to_do::{
     ItemTypes,
     to_do_factory,
+    structs::traits::create::Create,
 };
 
 fn main() {
-    let to_do_item = to_do_factory("pending", "make");
+    let to_do_item = to_do_factory("pending", "pending item");
     match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => println!("item: \"{}\" is pending.", item.super_struct.title),
+        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
         ItemTypes::Done(item) => println!("item: \"{}\" is done.", item.super_struct.title),
     }
 }
