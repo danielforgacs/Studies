aa17c54d97e924a07590ed7c72cfe0b7c42b8fc0
df@ <@>
2023-04-02 22:42:17 +0100

    finished get trait on page 56.

--- __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/traits/get.rs
+++ __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/traits/get.rs
@@ -1,5 +1,18 @@
+use serde_json::{
+    Map,
+    Value,
+};
+
 pub trait Get {
-    fn get(&self, title: &str) {
+    fn get(&self, title: &String, state: &Map<String, Value>) {
         println!("'{}' is being fetched.", title);
+        let item = state.get(title);
+        match item {
+            Some(result) => {
+                println!("Item: {}", title);
+                println!("status: {}\n\n", result);
+            }
+            None => println!("Item: {} was not found.", title),
+        }
     }
 }
