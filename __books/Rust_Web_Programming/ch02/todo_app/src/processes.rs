b24dd1fc6ee74044f9f676d3365233cd366a6b2d
df@ <@>
2023-04-04 05:44:26 +0100

    finished "process_input" on page 60

--- __books/Rust_Web_Programming/ch02/todo_app/src/processes.rs
+++ __books/Rust_Web_Programming/ch02/todo_app/src/processes.rs
@@ -19,13 +19,19 @@ fn process_pending(item: Pending, command: String, state: &mut Map<String, Value
     }
 }
 
-fn process_done(item: Pending, command: String, state: &mut Map<String, Value>) {
+fn process_done(item: Done, command: String, state: &mut Map<String, Value>) {
     let mut state = state.clone();
     match command.as_str() {
         "get" => item.get(&item.super_struct.title, &mut state),
-        "create" => item.create(&item.super_struct.title, &item.super_struct.status, &mut state),
         "delete" => item.delete(&item.super_struct.title, &mut state),
         "edit" => item.set_to_pending(&item.super_struct.title, &mut state),
         _ => println!(r#"command "{}" not supported"#, command),
+    }
+}
+
+pub fn process_input(item: ItemTypes, command: String, state: &mut Map<String, Value>) {
+    match item {
+        ItemTypes::Pending(item) => process_pending(item, command, state),
+        ItemTypes::Done(item) => process_done(item, command, state),
     }
 }
