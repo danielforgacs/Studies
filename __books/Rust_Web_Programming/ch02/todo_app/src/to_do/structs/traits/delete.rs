5b19fb1fd53dfb9aebaca9234ec906a142528b6e
df@ <@>
2023-04-04 05:38:55 +0100

    fixed delete trait method name.

--- __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/traits/delete.rs
+++ __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/traits/delete.rs
@@ -2,7 +2,7 @@ use serde_json::Map;
 use serde_json::value::Value;
 
 pub trait Delete {
-    fn get(&self, title: &str, state: &mut Map<String, Value>) {
+    fn delete(&self, title: &str, state: &mut Map<String, Value>) {
         println!("'{}' is being deleted.", title);
         state.remove(title);
         println!("Deleted: '{}'.", title);
