15f0cc9262623b84f2f5f3b1333730554f52bd88
df@ <@>
2023-04-04 05:24:04 +0100

    fixed edit trait.

--- __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/traits/edit.rs
+++ __books/Rust_Web_Programming/ch02/todo_app/src/to_do/structs/traits/edit.rs
@@ -1,9 +1,15 @@
+use serde_json::Map;
+use serde_json::Value;
+use serde_json::json;
+
 pub trait Edit {
-    fn set_to_done(&self, title: &str) {
+    fn set_to_done(&self, title: &str, state: &mut Map<String, Value>) {
         println!("'{}' is being set to done.", title);
+        state.insert(title.to_string(), json!("done".to_string()));
     }
 
-    fn set_to_pending(&self, title: &str) {
+    fn set_to_pending(&self, title: &str, state: &mut Map<String, Value>) {
         println!("'{}' is being set to pending.", title);
+        state.insert(title.to_string(), json!("pending".to_string()));
     }
 }
