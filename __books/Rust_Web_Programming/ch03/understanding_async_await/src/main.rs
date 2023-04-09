edbbebc10750f1622a3d316225098c3527d7da91
df@ <@>
2023-04-09 17:54:31 +0100

    ok

--- __books/Rust_Web_Programming/ch03/understanding_async_await/src/main.rs
+++ __books/Rust_Web_Programming/ch03/understanding_async_await/src/main.rs
@@ -1,3 +1,12 @@
+use std::time::Duration;
+use std::thread::sleep;
+
+async fn do_something(number: i8) -> i8 {
+    println!("number {} is running.", number);
+    let two_secs = Duration::new(2, 0);
+    sleep(two_secs);
+    return 2
+}
+
 fn main() {
-    println!("Hello, world!");
 }
