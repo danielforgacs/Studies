f2a1d389d3b69b11726d171129fbf5250465dc62
df@ <@>
2023-04-13 10:29:15 +0100

    configured views paths in the http server.

--- __books/Rust_Web_Programming/ch03/managing_views/src/main.rs
+++ __books/Rust_Web_Programming/ch03/managing_views/src/main.rs
@@ -3,7 +3,10 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new())
+    HttpServer::new(|| {
+        App::new()
+        .configure(views::views_factory)
+    })
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
