abd993e6a1b81f96c215ecd6cc9cda43d2cdfcd6
df@ <@>
2023-04-18 22:45:13 +0100

    wrote itemfactory path config and added it to the views factory.

--- __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/mod.rs
+++ __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/mod.rs
@@ -6,4 +6,5 @@ use actix_web::web;
 
 pub fn views_factory(app: &mut web::ServiceConfig) {
     auth::auth_factory(app);
+    to_do::item_factory(app);
 }
--- __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/to_do/mod.rs
+++ __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/to_do/mod.rs
@@ -1,1 +1,11 @@
 mod create;
+
+use actix_web::web;
+use crate::views::path::Path;
+
+
+pub fn item_factory(app: &mut web::ServiceConfig) {
+    let base_path = Path { prefix: String::from("/item") };
+
+    app.route(&base_path.define(String::from("/create/{title}")), web::get().to(create::create));
+}
