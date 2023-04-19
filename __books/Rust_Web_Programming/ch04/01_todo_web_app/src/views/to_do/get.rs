4243df8be29d8fb3e3e070f25e9cb9ad4437a676
df@ <@>
2023-04-19 21:29:02 +0100

    configured "/get" route.

--- __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/to_do/get.rs
+++ __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/to_do/get.rs
@@ -3,7 +3,7 @@ use serde_json::{Value, Map};
 use crate::state::read_file;
 use crate::PERSISTENCE_FILE_NAME;
 
-async fn get() -> impl Responder {
+pub async fn get() -> impl Responder {
     let state = read_file(PERSISTENCE_FILE_NAME);
     web::Json(state)
 }
