2fe2b9dcc3c878c88c3a8f1cb20f123c1ee8ca51
df@ <@>
2023-04-20 23:23:52 +0100

    started todo items api response struct.

---
+++ __books/Rust_Web_Programming/ch04/01_todo_web_app/src/json_serialization/to_do_items.rs
@@ -0,0 +1,11 @@
+use serde::Serialize;
+use crate::to_do::ItemTypes;
+use crate::to_do::structs::base::Base;
+
+#[derive(Serialize)]
+pub struct ToDoItems {
+    pub pending_items: Vec<Base>,
+    pub done_items: Vec<Base>,
+    pub pending_item_count: u8,
+    pub donr_item_count: u8,
+}
