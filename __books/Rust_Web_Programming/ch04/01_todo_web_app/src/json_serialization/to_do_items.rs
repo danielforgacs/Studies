ed44e13fe9ee0ffd371764a62a541802b80616b2
df@ <@>
2023-04-21 21:25:18 +0100

    wrote todo items struct new()

--- __books/Rust_Web_Programming/ch04/01_todo_web_app/src/json_serialization/to_do_items.rs
+++ __books/Rust_Web_Programming/ch04/01_todo_web_app/src/json_serialization/to_do_items.rs
@@ -1,15 +1,35 @@
-use serde::Serialize;
-use crate::to_do::ItemTypes;
 use crate::to_do::structs::base::Base;
+use crate::to_do::ItemTypes;
+use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct ToDoItems {
     pub pending_items: Vec<Base>,
     pub done_items: Vec<Base>,
-    pub pending_item_count: u8,
-    pub donr_item_count: u8,
+    pub pending_item_count: i8,
+    pub donr_item_count: i8,
 }
 
 impl ToDoItems {
-    pub
+    pub fn new(input_items: Vec<ItemTypes>) -> ToDoItems {
+        let mut pending_arraw_buffer = Vec::new();
+        let mut done_arraw_buffer = Vec::new();
+
+        for item in input_items {
+            match item {
+                ItemTypes::Pending(packed) => pending_arraw_buffer.push(packed.super_struct),
+                ItemTypes::Done(packed) => done_arraw_buffer.push(packed.super_struct),
+            }
+        }
+
+        let pending_count = pending_arraw_buffer.len() as i8;
+        let done_count = done_arraw_buffer.len() as i8;
+
+        ToDoItems {
+            pending_items: pending_arraw_buffer,
+            done_items: done_arraw_buffer,
+            pending_item_count: pending_count,
+            donr_item_count: done_count,
+        }
+    }
 }
