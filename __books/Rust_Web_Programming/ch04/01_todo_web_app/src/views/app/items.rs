017804803c7d1f5daf8f87b5e8b09f7ad2301cfb
df@ <@>
2023-04-29 17:19:16 +0100

    serving main.html content in the app.

--- __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/app/items.rs
+++ __books/Rust_Web_Programming/ch04/01_todo_web_app/src/views/app/items.rs
@@ -1,7 +1,9 @@
+use super::content_loader::read_file;
 use actix_web::HttpResponse;
 
 pub async fn items() -> HttpResponse {
+    let html_data = read_file("./templates/main.html");
     HttpResponse::Ok()
     .content_type("text/html; charset=utf-8")
-    .body("<h1>Items</h1>")
+    .body(html_data)
 }
