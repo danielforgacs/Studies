05dfd39542eb4fbd6d85a33a6a7d2a37012fd95d
df@ <@>
2023-05-09 23:57:55 +0100

    Display trait for TaskStatus.

--- __books/Rust_Web_Programming/todo_app/src/to_do/enums.rs
+++ __books/Rust_Web_Programming/todo_app/src/to_do/enums.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter, Result};
+
 pub enum TaskStatus {
     Done,
     Pending,
@@ -8,6 +10,15 @@ impl TaskStatus {
         match &self {
             &Self::Done => {"DONE".to_string()},
             &Self::Pending => {"PENDING".to_string()}
+        }
+    }
+}
+
+impl Display for TaskStatus {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match &self {
+            Self::Done => write!(f, "Done"),
+            Self::Pending => write!(f, "Pending"),
         }
     }
 }
