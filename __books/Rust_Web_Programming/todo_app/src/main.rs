5ba8fcc9a19d8f09d9fc74a137c2bb9e2d65a106
df@ <@>
2023-05-11 02:08:18 +0100

    wrote CRUD traits demo in main.

--- __books/Rust_Web_Programming/todo_app/src/main.rs
+++ __books/Rust_Web_Programming/todo_app/src/main.rs
@@ -2,19 +2,20 @@ mod to_do;
 use to_do::to_do_factory;
 use to_do::enums::TaskStatus;
 use to_do::ItemTypes;
+use to_do::traits::get::Get;
+use to_do::traits::delete::Delete;
+use to_do::traits::edit::Edit;
 
 fn main() {
     let to_do_item = to_do_factory("washing", TaskStatus::DONE);
     match to_do_item {
         ItemTypes::Done(item) => {
-            println!("{}", item.super_struct.title);
-            println!("{}", item.super_struct.status);
-            println!("{}", item.super_struct.status.stringify());
+            item.get(&item.super_struct.title);
+            item.delete(&item.super_struct.title);
         },
         ItemTypes::Pending(item) => {
-            println!("{}", item.super_struct.title);
-            println!("{}", item.super_struct.status);
-            println!("{}", item.super_struct.status.stringify());
+            item.get(&item.super_struct.title);
+            item.set_to_done(&item.super_struct.title);
         }
     }
 }
