9462314fd4e2a5f244d82a9f03203e6bff638e2f
df@ <@>
2023-05-11 02:04:23 +0100

    implemented CRUD traits for structs

--- __books/Rust_Web_Programming/todo_app/src/to_do/structs/pending.rs
+++ __books/Rust_Web_Programming/todo_app/src/to_do/structs/pending.rs
@@ -1,5 +1,9 @@
 use super::base::Base;
 use super::super::enums::TaskStatus;
+use super::super::traits::get::Get;
+use super::super::traits::edit::Edit;
+use super::super::traits::create::Create;
+
 
 pub struct Pending {
     pub super_struct: Base,
@@ -14,3 +18,7 @@ impl Pending {
         Self { super_struct: base }
     }
 }
+
+impl Get for Pending {}
+impl Edit for Pending {}
+impl Create for Pending {}
