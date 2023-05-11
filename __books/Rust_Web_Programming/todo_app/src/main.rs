35dc0d747543047ea02890cebecb28ac6c43e556
df@ <@>
2023-05-11 01:46:45 +0100

    main stuff

--- __books/Rust_Web_Programming/todo_app/src/main.rs
+++ __books/Rust_Web_Programming/todo_app/src/main.rs
@@ -1,14 +1,22 @@
 mod to_do;
 use to_do::structs::done::Done;
 use to_do::structs::pending::Pending;
+use to_do::to_do_factory;
+use to_do::enums::TaskStatus;
+use to_do::ItemTypes;
 
 fn main() {
-    let done = Done::new("shopping");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status);
-    println!("{}", done.super_struct.status.stringify());
-    let landry = Pending::new("landry");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status);
-    println!("{}", done.super_struct.status.stringify());
+    let to_do_item = to_do_factory("washing", TaskStatus::Done);
+    match to_do_item {
+        ItemTypes::Done(item) => {
+            println!("{}", item.super_struct.title);
+            println!("{}", item.super_struct.status);
+            println!("{}", item.super_struct.status.stringify());
+        },
+        ItemTypes::Pending(item) => {
+            println!("{}", item.super_struct.title);
+            println!("{}", item.super_struct.status);
+            println!("{}", item.super_struct.status.stringify());
+        }
+    }
 }
