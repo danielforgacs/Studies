72b4d0e06cbfc04895982e7a76d597109c8f5ff6
df@ <@>
2023-05-15 22:03:37 +0100

    ok

--- Errors-rs/src/main.rs
+++ Errors-rs/src/main.rs
@@ -7,20 +7,20 @@ fn main() {
 fn action_one() -> Result<u8> {
     println!("action_one");
     let problem: Result<()> = Err(Error::new(ErrorKind::Other, "Error ONE!"));
-    problem.map_err(|e| {dbg!(e)}).ok();
+    problem.map_err(|e| {println!("shit happend ONE: {}", e)}).ok();
     Ok(1)
 }
 
 fn action_two(_n: u8) -> Result<u8> {
     println!("action_two");
     let problem: Result<()> = Err(Error::new(ErrorKind::Other, "Error TWO!"));
-    problem.map_err(|e| {dbg!(e)}).ok();
+    problem.map_err(|e| {println!("shit happend TWO: {}", e)}).ok();
     Ok(2)
 }
 
 fn action_three(_n: u8) -> Result<()> {
     println!("action_three");
     let problem: Result<()> = Err(Error::new(ErrorKind::Other, "Error THREE!"));
-    problem.map_err(|e| {dbg!(e)}).ok();
+    problem.map_err(|e| {println!("shit happend THREE: {}", e)}).ok();
     Ok(())
 }
