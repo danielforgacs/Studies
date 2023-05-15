ec1ce86043066d4550a0f3b82a923c4ce3523c77
df@ <@>
2023-05-15 20:59:45 +0100

    ok

--- Errors-rs/src/main.rs
+++ Errors-rs/src/main.rs
@@ -1,9 +1,17 @@
 use std::io::{Error, Result, ErrorKind};
 
 fn main() {
-    action_one();
+    action_one().and_then(action_two).and_then(action_three).ok();
 }
 
-fn action_one() -> Result<()> {
+fn action_one() -> Result<u8> {
+    Ok(1)
+}
+
+fn action_two(_n: u8) -> Result<u8> {
+    Ok(2)
+}
+
+fn action_three(_n: u8) -> Result<()> {
     Ok(())
 }
