6904bcde9a160856e78a03716ee6e680f50e60bb
df@ <@>
2023-05-16 22:48:54 +0100

    main.

--- __books/Rust_Web_Programming/todo_app/src/main.rs
+++ __books/Rust_Web_Programming/todo_app/src/main.rs
@@ -7,17 +7,33 @@ use to_do::ItemTypes;
 use to_do::traits::get::Get;
 use to_do::traits::delete::Delete;
 use to_do::traits::edit::Edit;
+use std::env;
+use state::{write_to_file, read_file};
+use serde_json::value::Value;
+use serde_json::{Map, json};
 
 fn main() {
-    let to_do_item = to_do_factory("washing", TaskStatus::DONE);
-    match to_do_item {
-        ItemTypes::Done(item) => {
-            item.get(&item.super_struct.title);
-            item.delete(&item.super_struct.title);
-        },
-        ItemTypes::Pending(item) => {
-            item.get(&item.super_struct.title);
-            item.set_to_done(&item.super_struct.title);
+    {
+        let to_do_item = to_do_factory("washing", TaskStatus::DONE);
+        match to_do_item {
+            ItemTypes::Done(item) => {
+                item.get(&item.super_struct.title);
+                item.delete(&item.super_struct.title);
+            },
+            ItemTypes::Pending(item) => {
+                item.get(&item.super_struct.title);
+                item.set_to_done(&item.super_struct.title);
+            }
         }
+    }
+    {
+        let args: Vec<String> = env::args().collect();
+        let status: &String = &args[1];
+        let title: &String = &args[2];
+        let mut state: Map<String, Value> = read_file("./state.json");
+        println!("Before operation: {:?}", state);
+        state.insert(title.to_string(), json!(status));
+        println!("After operation: {:?}", state);
+        write_to_file("./state.json", &mut state);
     }
 }
