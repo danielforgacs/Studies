ba29a3015b0e1780246a3855f736443c4b63f679
df@ <@>
2023-05-17 08:05:41 +0100

    clean up.

--- __books/Rust_Web_Programming/todo_app/src/to_do/traits/create.rs
+++ __books/Rust_Web_Programming/todo_app/src/to_do/traits/create.rs
@@ -6,7 +6,7 @@ pub trait Create {
     fn create(&self, title: &String, status: &String, state: &mut Map<String, Value>) {
         println!("{} is being created", title);
         state.insert(title.to_string(), json!(status));
-        write_to_file("./status.json", state);
-        println!("\n\n{} is being created\n\n", title);
+        write_to_file("./state.json", state);
+        println!("{} is saved.", title);
     }
 }
