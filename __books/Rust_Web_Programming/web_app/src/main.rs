09b86247d18b6f0d9a9fd63590c7bdfff08cb61d
df@ <@>
2023-05-17 20:04:18 +0100

    wrote the server.

--- __books/Rust_Web_Programming/web_app/src/main.rs
+++ __books/Rust_Web_Programming/web_app/src/main.rs
@@ -1,4 +1,16 @@
 mod views;
 
-fn main() {
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new( || App::new()
+
+    )
+    .bind(("0.0.0.0", 8080)).map_err(|err| {
+        println!("Error binding: {}", err);
+        err
+    })?
+    .run()
+    .await
 }
