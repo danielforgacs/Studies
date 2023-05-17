4c1bd3dbe4b6e6a4e6dbdf505bac55f69a70857f
df@ <@>
2023-05-17 20:19:53 +0100

    wrote login, logout view, view cfg factories and configured the server app.

--- __books/Rust_Web_Programming/web_app/src/main.rs
+++ __books/Rust_Web_Programming/web_app/src/main.rs
@@ -4,8 +4,9 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new( || App::new()
-
+    HttpServer::new( ||
+        App::new()
+            .configure(views::views_factory)
     )
     .bind(("0.0.0.0", 8080)).map_err(|err| {
         println!("Error binding: {}", err);
