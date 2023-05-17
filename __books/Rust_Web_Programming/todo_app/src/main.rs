c0553914a39fe3ecae97063ef83d3b957ff3bb6f
df@ <@>
2023-05-17 08:13:20 +0100

    added process input entry point.

--- __books/Rust_Web_Programming/todo_app/src/main.rs
+++ __books/Rust_Web_Programming/todo_app/src/main.rs
@@ -18,16 +18,8 @@ fn main() {
         let args: Vec<String> = env::args().collect();
         let status: &String = &args[1];
         let title: &String = &args[2];
-        let item_type = to_do_factory(title, TaskStatus::PENDING);
-        let item = match item_type {
-            ItemTypes::Pending(item) => item,
-            _ => panic!("NOT PROCESSED YET."),
-        };
-        let mut state: Map<String, Value> = read_file("./state.json");
-        processes::process_pending(item, "create".to_string(), &mut state);
-        // println!("Before operation: {:?}", state);
-        // state.insert(title.to_string(), json!(status));
-        // println!("After operation: {:?}", state);
-        // write_to_file("./state.json", &mut state);
+        let item = to_do_factory(title, TaskStatus::PENDING);
+        let mut state = read_file("./state.json");
+        processes::process_input(item, "create".to_string(), &mut state);
     }
 }
