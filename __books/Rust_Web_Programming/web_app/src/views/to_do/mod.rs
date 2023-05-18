32e9ed4149f20c6f71d77f20f77d5e5e3e608a72
df@ <@>
2023-05-18 08:29:20 +0100

    fixed url

--- __books/Rust_Web_Programming/web_app/src/views/to_do/mod.rs
+++ __books/Rust_Web_Programming/web_app/src/views/to_do/mod.rs
@@ -4,7 +4,7 @@ use actix_web::web::{ServiceConfig, get, scope};
 
 pub fn to_do_views_factory(app: &mut ServiceConfig) {
     app.service(
-        scope("v1/item/")
+        scope("v1/item")
             .route("create/{title}", get().to(create::create))
     );
 }
