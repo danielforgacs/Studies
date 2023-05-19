f591ed5cc0f8ce84b604777c76657c1a3e9320b3
df@ <@>
2023-05-19 21:07:58 +0100

    implemented responder for ToDoItems; refactored get view.

--- __books/Rust_Web_Programming/web_app/src/views/to_do/get.rs
+++ __books/Rust_Web_Programming/web_app/src/views/to_do/get.rs
@@ -6,13 +6,17 @@ use crate::TaskStatus;
 use crate::to_do::to_do_factory;
 use crate::json_serialization::to_do_items::TodoItems;
 
+// pub async fn get() -> impl Responder {
+//     let state: Map<String, Value> = read_file("./state.json");
+//     let mut array_buffer = Vec::new();
+//     for (key, value) in state {
+//         let status = TaskStatus::from(value.to_string().replace('\"', ""));
+//         let item = to_do_factory(&key, status);
+//         array_buffer.push(item);
+//     }
+//     Json(TodoItems::new(array_buffer))
+// }
+
 pub async fn get() -> impl Responder {
-    let state: Map<String, Value> = read_file("./state.json");
-    let mut array_buffer = Vec::new();
-    for (key, value) in state {
-        let status = TaskStatus::from(value.to_string().replace('\"', ""));
-        let item = to_do_factory(&key, status);
-        array_buffer.push(item);
-    }
-    Json(TodoItems::new(array_buffer))
+    TodoItems::get_state()
 }
