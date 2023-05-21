3968a022d69fecd498304a0f7b4174c1a31b68fe
df@ <@>
2023-05-21 12:13:59 +0100

    started 1st middleware.

--- __books/Rust_Web_Programming/web_app/src/main.rs
+++ __books/Rust_Web_Programming/web_app/src/main.rs
@@ -6,11 +6,20 @@ mod json_serialization;
 
 use to_do::enums::TaskStatus;
 use actix_web::{App, HttpServer};
+use actix_service::Service;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new( ||
         App::new()
+            .wrap_fn(|req, srv| {
+                println!("request: {:?}", req);
+                let future = srv.call(req);
+                async {
+                    let result = future.await?;
+                    Ok(result)
+                }
+            })
             .configure(views::views_factory)
     )
     .bind(("0.0.0.0", 8080)).map_err(|err| {
