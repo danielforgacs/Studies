5470931431d4e297821e16757e82003aed779116
df@ <@>
2023-05-21 10:59:46 +0100

    clean up

--- __books/Rust_Web_Programming/web_app/src/views/to_do/get.rs
+++ __books/Rust_Web_Programming/web_app/src/views/to_do/get.rs
@@ -1,17 +1,6 @@
 use actix_web::Responder;
 use crate::json_serialization::to_do_items::ToDoItems;
 
-// pub async fn get() -> impl Responder {
-//     let state: Map<String, Value> = read_file("./state.json");
-//     let mut array_buffer = Vec::new();
-//     for (key, value) in state {
-//         let status = TaskStatus::from(value.to_string().replace('\"', ""));
-//         let item = to_do_factory(&key, status);
-//         array_buffer.push(item);
-//     }
-//     Json(TodoItems::new(array_buffer))
-// }
-
 pub async fn get() -> impl Responder {
     ToDoItems::get_state()
 }
