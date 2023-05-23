553358dab7dee062620337a9f7255b4f93ed83a1
df@ <@>
2023-05-23 18:49:12 +0100

    started app/items view.

--- __books/Rust_Web_Programming/web_app/src/views/mod.rs
+++ __books/Rust_Web_Programming/web_app/src/views/mod.rs
@@ -5,8 +5,10 @@ mod app;
 use actix_web::web::{ServiceConfig};
 use auth::auth_views_factory;
 use to_do::to_do_views_factory;
+use app::app_views_factory;
 
 pub fn views_factory(app: &mut ServiceConfig) {
+    app_views_factory(app);
     auth_views_factory(app);
     to_do_views_factory(app);
 }
