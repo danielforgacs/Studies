9e862d3fc334c3e98f50f5b35cba90127fd80591
df@ <@>
2023-05-23 18:50:51 +0100

    added empty scope for app view

--- __books/Rust_Web_Programming/web_app/src/views/app/mod.rs
+++ __books/Rust_Web_Programming/web_app/src/views/app/mod.rs
@@ -3,9 +3,8 @@ mod items;
 use actix_web::web::{ServiceConfig, get, post, scope};
 
 pub fn app_views_factory(app: &mut ServiceConfig) {
-    // app.service(
-    //     scope("")
-    //     .route("", get().to(items::items))
-    // );
-    app.route("/", get().to(items::items));
+    app.service(
+        scope("/")
+        .route("", get().to(items::items))
+    );
 }
