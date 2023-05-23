025bb3dacc1e04d5e4a2821574a3a7ede9e08d0d
df@ <@>
2023-05-23 20:13:00 +0100

    create api view not returns the state.

--- __books/Rust_Web_Programming/web_app/src/views/to_do/create.rs
+++ __books/Rust_Web_Programming/web_app/src/views/to_do/create.rs
@@ -1,14 +1,16 @@
-use actix_web::HttpRequest;
+use actix_web::{HttpRequest, HttpResponse};
 use serde_json::{Map, Value};
 use crate::state::read_file;
 use crate::to_do::to_do_factory;
 use crate::to_do::enums::TaskStatus;
 use crate::processes::process_input;
+use crate::json_serialization::to_do_items::ToDoItems;
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> HttpResponse {
     let state: Map<String, Value> = read_file("./state.json");
     let title = req.match_info().get("title").unwrap().to_string();
     let item = to_do_factory(&title, TaskStatus::PENDING);
     process_input(item, "create".to_string(), &state);
-    format!("{} created", title)
+    HttpResponse::Ok()
+        .json(ToDoItems::get_state())
 }
