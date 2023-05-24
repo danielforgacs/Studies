85ad1f0cbda80d0c7c9d4973db477f75a881468c
df@ <@>
2023-05-24 20:17:16 +0100

    ...

--- __books/Rust_Web_Programming/web_app/javascript/main.js
+++ __books/Rust_Web_Programming/web_app/javascript/main.js
@@ -26,6 +26,8 @@ function apiCall(url, method) {
             console.log('--> this.responseText:', this.responseText)
             renderItems(JSON.parse(this.responseText)['pending_items'], 'edit', 'pendingItems', editItem)
             renderItems(JSON.parse(this.responseText)['done_items'], 'delete', 'doneItems', deleteItem)
+            document.getElementById('completedNum').innerHTML = JSON.parse(this.responseText)['done_count']
+            document.getElementById('pendingNum').innerHTML = JSON.parse(this.responseText)['pending_count']
         }
     })
     xhr.open(method, url)
