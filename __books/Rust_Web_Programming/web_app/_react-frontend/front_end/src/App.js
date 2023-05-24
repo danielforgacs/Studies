cb5152e5c908c47a197aef679ccee06474da3e82
df@ <@>
2023-05-24 22:24:53 +0100

    App update.

--- __books/Rust_Web_Programming/web_app/_react-frontend/front_end/src/App.js
+++ __books/Rust_Web_Programming/web_app/_react-frontend/front_end/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    "message": "To Do"
+  }
+  render() {
+    return (
+      <div className='App'>
+        <p>{this.state.message} Application</p>
+      </div>
+    )
+  }
 }
 
 export default App;
