8c8140d4d1b7f5447c5bbc2da3c5245d2a31f954
df@ <@>
2023-05-24 20:17:16 +0100

    ...

--- __books/Rust_Web_Programming/web_app/src/views/app/items.rs
+++ __books/Rust_Web_Programming/web_app/src/views/app/items.rs
@@ -1,5 +1,6 @@
 use actix_web::HttpResponse;
 use super::content_loader::read_file;
+use super::content_loader::add_component;
 
 pub async fn items() -> HttpResponse {
     let mut html_data = read_file("./templates/main.html");
@@ -12,6 +13,8 @@ pub async fn items() -> HttpResponse {
 
     let css_data = read_file("./css/main.css");
     html_data = html_data.replace("{{CSS}}", &css_data);
+
+    html_data = add_component(String::from("header"), html_data);
 
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
