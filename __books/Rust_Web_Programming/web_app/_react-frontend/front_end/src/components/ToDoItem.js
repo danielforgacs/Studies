6f490b5aa582ae6bac3e976a989e570dbe0a9a05
df@ <@>
2023-05-25 21:48:50 +0100

    @chapter 5 - ref: 10.155

---
+++ __books/Rust_Web_Programming/web_app/_react-frontend/front_end/src/components/ToDoItem.js
@@ -0,0 +1,53 @@
+import React, {Component } from "react";
+import axios from "axios";
+
+class ToDoItem extends Component {
+    state = {
+        "title": this.props.title,
+        "status": this.props.status,
+        "button": this.props.processStatus(this.props.status),
+    }
+
+    processStatus(status) {
+        if (status === "PENDING") {
+            return "edit"
+        } else {
+            return "delete"
+        }
+    }
+    
+    inverseStatus(status) {
+        if (status === "PENDING") {
+            return "DONE"
+        } else {
+            return "PENDING"
+        }
+    }
+
+    sendRequest = () => {
+        axios.post("http://127.0.0.1:8080/v1/item/"+this.state.button,
+            {
+                "title": this.state.title,
+                "status": this.state.status,
+            },
+            {
+                "headers": {
+                    "user-token": "React ToDoItem token"
+                }
+        })
+        .then(response => {
+            this.props.passBackResponse(response)
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <p>{this.state.title</p>
+                <button onClick={this.sendRequest}>{this.state.button}</button>
+            </div>
+        )
+    }
+}
+
+export default ToDoItem
