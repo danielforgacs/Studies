4b3d2439c8ee9a1aa7d1298c2a18f43b7948d168
df@ <@>
2023-05-25 20:50:34 +0100

    added cors to app

--- __books/Rust_Web_Programming/web_app/src/main.rs
+++ __books/Rust_Web_Programming/web_app/src/main.rs
@@ -8,21 +8,27 @@ mod jwt;
 use to_do::enums::TaskStatus;
 use actix_web::{App, HttpServer};
 use actix_service::Service;
+use actix_cors;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new( ||
+    HttpServer::new( || {
+        let cors = actix_cors::Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
         App::new()
-            // .wrap_fn(|req, srv| {
-            //     println!("request: {:?}", req);
-            //     let future = srv.call(req);
-            //     async {
-            //         let result = future.await?;
-            //         Ok(result)
-            //     }
-            // })
-            .configure(views::views_factory)
-    )
+        .configure(views::views_factory)
+        .wrap(cors)
+        // .wrap_fn(|req, srv| {
+        //     println!("request: {:?}", req);
+        //     let future = srv.call(req);
+        //     async {
+        //         let result = future.await?;
+        //         Ok(result)
+        //     }
+        // })
+    })
     .bind(("0.0.0.0", 8080)).map_err(|err| {
         println!("Error binding: {}", err);
         err
