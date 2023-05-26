f1d1be9d0e6a64f3c4f96d7ee93a5a2babc0cdfc
df@ <@>
2023-05-26 18:40:08 +0100

    fixed token name

--- __books/Rust_Web_Programming/react_fron_end/src/components/CreateToDoItem.js
+++ __books/Rust_Web_Programming/react_fron_end/src/components/CreateToDoItem.js
@@ -13,7 +13,7 @@ class CreateToDoItem extends Component {
         axios.post("http://127.0.0.1:8080/v1/item/create/" +
             this.state.title,
             {},
-            {headers: {"token": "some_token"}})
+            {headers: {"user-token": "some_token"}})
             .then(response => {
                 this.setState({"title": ""});
                 this.props.passBackResponse(response);
