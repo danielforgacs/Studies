c8aa67af84f1e12bc90dd0ffb001720c982f8c7c
df@ <@>
2023-05-27 21:39:33 +0100

    started script to start the db

---
+++ __books/Rust_Web_Programming/web_app/wait_for_database.sh
@@ -0,0 +1,11 @@
+# in the book: scripts/wait_for_database.sh
+# cd ..
+
+docker-compose up -d
+until pg_isready -h localhost -p 5432 -U username
+do
+  echo "Waiting for postgres"
+  sleep 2;
+done
+echo "docker is now running"
+docker-compose down
