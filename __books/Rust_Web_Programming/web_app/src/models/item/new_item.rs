1c7f4196a325c58a2fcf94ab78acc5250d6749db
df@ <@>
2023-06-21 00:09:31 +0100

    created table insertable new item srtruct.

---
+++ __books/Rust_Web_Programming/web_app/src/models/item/new_item.rs
@@ -0,0 +1,21 @@
+use diesel::prelude::*;
+use crate::schema::to_do;
+use chrono::{NaiveDateTime, Utc};
+
+#[derive(Insertable)]
+#[table_name="to_do"]
+pub struct NewItem {
+    pub title: String,
+    pub status: String,
+    pub date: NaiveDateTime,
+}
+
+impl NewItem {
+    pub fn new(title: String) -> Self {
+        Self {
+            title,
+            status: String::from("PENDING"),
+            date: Utc::now().naive_local(),
+        }
+    }
+}
