63a004ce5d6fb0dcb1cfe0739aafdf3b1f8052db
df@ <@>
2023-07-18 00:08:16 +0100

    ok

--- __books/Command-Line_Rust/ch02-echo-rs/src/main.rs
+++ __books/Command-Line_Rust/ch02-echo-rs/src/main.rs
@@ -4,6 +4,5 @@ fn main() {
     let result = args()
     .map(|mut i| {i.push(' '); i})
     .collect::<Vec<String>>();
-    dbg!(&result);
-    // println!("{}", result);
+    println!("{:?}", result);
 }
