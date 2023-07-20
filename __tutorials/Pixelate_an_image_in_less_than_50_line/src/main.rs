805fcbd242b4a4257289f243e91ee1c6f1493403
df@ <@>
2023-07-20 23:39:59 +0100

    reading image ok.

--- __tutorials/Pixelate_an_image_in_less_than_50_line/src/main.rs
+++ __tutorials/Pixelate_an_image_in_less_than_50_line/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use image::io::Reader as ImageReader;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let img = ImageReader::open("images/noaa-IooaujyvkVk-unsplash.jpg")?.decode()?;
+    Ok(())
 }
