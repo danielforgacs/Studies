7b1ee84642eef5d1fe3e7571da3737c74d27f7f9
df@ <.@.>
2023-09-18 23:31:19 +0100

    ok

--- generics/src/generic_struct.rs
+++ generics/src/generic_struct.rs
@@ -2,14 +2,18 @@ struct AnyNum<T> {
     numbers: Vec<T>
 }
 
+impl<T> AnyNum<T> {
+    fn new(nums: Vec<T>) -> Self {
+        Self {
+            numbers: nums,
+        }
+    }
+}
+
 pub fn generic_struct() {
     print!("Generic struct");
-    let stuff_0 = AnyNum {
-        numbers: vec![1_u8, 2_u8, 3_u8],
-    };
+    let stuff_0 = AnyNum::new(vec![1_u8, 2_u8, 3_u8]);
     dbg!(stuff_0.numbers);
-    let stuff_1 = AnyNum {
-        numbers: vec![1_f32, 2_f32, 3_f32],
-    };
+    let stuff_1 = AnyNum::new(vec![1_f32, 2_f32, 3_f32]);
     dbg!(stuff_1.numbers);
 }
