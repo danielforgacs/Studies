91a4aaae5737cd5e39b1de59854e5cc281f28da7
df@ <.@.>
2023-09-18 22:58:56 +0100

    ok

--- generics/src/multiple_types_in_vector.rs
+++ generics/src/multiple_types_in_vector.rs
@@ -1,4 +1,7 @@
 use core::fmt::Debug;
+use std::fmt::Formatter;
+
+trait GroupThem {}
 
 pub fn vec_multi_type() {
     let mut stuff: Vec<Box<dyn Debug>> = Vec::new();
@@ -9,4 +12,28 @@ pub fn vec_multi_type() {
     stuff.push(Box::new("str"));
     stuff.push(Box::new("String".to_string()));
     dbg!(&stuff);
+}
+
+pub fn vec_multi_type_w_trait() {
+    let mut stuff: Vec<Box<dyn GroupThem>> = Vec::new();
+    stuff.push(Box::new(1_u8));
+    stuff.push(Box::new(2_u16));
+    stuff.push(Box::new(3.0_f32));
+    stuff.push(Box::new(4.0_f64));
+    stuff.push(Box::new("str"));
+    stuff.push(Box::new("String".to_string()));
+    dbg!(&stuff);
+}
+
+impl GroupThem for u8 {}
+impl GroupThem for u16 {}
+impl GroupThem for f32 {}
+impl GroupThem for f64 {}
+impl GroupThem for &str {}
+impl GroupThem for String {}
+
+impl Debug for dyn GroupThem {
+    fn fmt(&self, _: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        todo!()
+    }
 }
