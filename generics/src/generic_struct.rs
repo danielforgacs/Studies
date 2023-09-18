eb463f18e16fd21464414453aada1614ffe3a743
df@ <.@.>
2023-09-18 23:23:46 +0100

    ok

--- generics/src/generic_struct.rs
+++ generics/src/generic_struct.rs
@@ -7,7 +7,9 @@ pub fn generic_struct() {
     let stuff_0 = AnyNum {
         numbers: vec![1_u8, 2_u8, 3_u8],
     };
+    dbg!(stuff_0.numbers);
     let stuff_1 = AnyNum {
         numbers: vec![1_f32, 2_f32, 3_f32],
     };
+    dbg!(stuff_1.numbers);
 }
