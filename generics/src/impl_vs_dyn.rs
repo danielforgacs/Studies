10a7cffbe2102619031383bda477120d27793c16
df@ <.@.>
2023-09-20 04:11:05 +0100

    ok

---
+++ generics/src/impl_vs_dyn.rs
@@ -0,0 +1,26 @@
+trait Rnd {}
+
+/// This syntax is coming from the compiler error
+trait AutoTraitForDyn: Rnd + std::fmt::Display {}
+
+impl Rnd for u8 {}
+impl Rnd for f32 {}
+
+impl AutoTraitForDyn for u8 {}
+impl AutoTraitForDyn for f32 {}
+
+pub fn impl_vs_dyn() {
+    trait_as_impl_arg(1_u8);
+    trait_as_impl_arg(1.0_f32);
+    trait_as_dyn_arg(&1_u8);
+    trait_as_dyn_arg(&1.0_f32);
+}
+
+fn trait_as_impl_arg(k: impl Rnd + std::fmt::Display) {
+    println!("trait_as_impl_arg: {}", k);
+}
+
+/// "dyn" alwalys has to be &
+fn trait_as_dyn_arg(k: &dyn AutoTraitForDyn) {
+    println!("trait_as_dyn_arg: {}", k);
+}
--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,10 +1,12 @@
 mod basic;
 mod multiple_types_in_vector;
 mod generic_struct;
+mod impl_vs_dyn;
 
 fn main() {
     basic::run_basic();
     multiple_types_in_vector::vec_multi_type();
     multiple_types_in_vector::vec_multi_type_w_trait();
     generic_struct::generic_struct();
+    impl_vs_dyn::impl_vs_dyn()
 }
