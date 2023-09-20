fd27fd31b7e62fea6155d58371f6fe3498c22d34
df@ <.@.>
2023-09-20 11:45:18 +0100

    nested generic structs

---
+++ generics/src/generic_nested_structs.rs
@@ -0,0 +1,33 @@
+struct MainEntity {
+    rows: Vec<DataType>,
+}
+
+enum DataType {
+    U8(Data<u8>),
+    F32(Data<f32>),
+}
+
+struct Data<T> {
+    values: Vec<T>,
+}
+
+pub fn generic_nested_structs() {
+    let row1 = DataType::U8(Data { values: vec![1_u8, 2_u8, 5_u8] });
+    let row2 = DataType::F32(Data { values: vec![1.0_f32, 2.0_f32, 5.0_f32] });
+    let group = MainEntity { rows: vec![row1, row2] };
+    for row_values in group.rows {
+        match row_values {
+            DataType::U8(values) => do_stuff_with_values::<u8>(values),
+            DataType::F32(values) => do_stuff_with_values::<f32>(values),
+        }
+    }
+}
+
+fn do_stuff_with_values<T>(values: Data<T>)
+where
+    T: std::fmt::Debug
+{
+    for k in values.values {
+        println!("{:?}", k);
+    }
+}
