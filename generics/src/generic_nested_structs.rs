0a97a9a43d0827c58ffe3e3e171ebfc3287a31d2
df@ <.@.>
2023-09-20 11:50:31 +0100

    ok

--- generics/src/generic_nested_structs.rs
+++ generics/src/generic_nested_structs.rs
@@ -11,23 +11,23 @@ struct Data<T> {
     values: Vec<T>,
 }
 
+impl<T: std::fmt::Display + std::fmt::Debug> Data<T> {
+    fn duplicate_and_print(&self) {
+        for value in &self.values {
+            print!("{} <{:?}>, ", value, value);
+        }
+    }
+}
+
 pub fn generic_nested_structs() {
+    println!("generic_nested_structs");
     let row1 = DataType::U8(Data { values: vec![1_u8, 2_u8, 5_u8] });
     let row2 = DataType::F32(Data { values: vec![1.0_f32, 2.0_f32, 5.0_f32] });
     let group = MainEntity { rows: vec![row1, row2] };
     for row_values in group.rows {
         match row_values {
-            DataType::U8(values) => do_stuff_with_values::<u8>(values),
-            DataType::F32(values) => do_stuff_with_values::<f32>(values),
+            DataType::U8(values) => values.duplicate_and_print(),
+            DataType::F32(values) => values.duplicate_and_print(),
         }
-    }
-}
-
-fn do_stuff_with_values<T>(values: Data<T>)
-where
-    T: std::fmt::Debug
-{
-    for k in values.values {
-        println!("{:?}", k);
     }
 }
