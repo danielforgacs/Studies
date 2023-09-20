394d9d9228fc6b112a03c736e1ddcd740a8e88e6
df@ <.@.>
2023-09-20 11:42:49 +0100

    clippy fix.

--- generics/src/multiple_types_in_vector.rs
+++ generics/src/multiple_types_in_vector.rs
@@ -15,14 +15,15 @@ pub fn vec_multi_type() {
 }
 
 pub fn vec_multi_type_w_trait() {
-    let mut stuff: Vec<Box<dyn GroupThem>> = Vec::new();
-    stuff.push(Box::new(1_u8));
-    stuff.push(Box::new(2_u16));
-    stuff.push(Box::new(3.0_f32));
-    stuff.push(Box::new(4.0_f64));
-    stuff.push(Box::new("str"));
-    stuff.push(Box::new("String".to_string()));
-    dbg!(&stuff);
+    let mut _stuff: Vec<Box<dyn GroupThem>> = vec![
+        Box::new(1_u8),
+        Box::new(2_u16),
+        Box::new(3.0_f32),
+        Box::new(4.0_f64),
+        Box::new("str"),
+        Box::new("String".to_string()),
+    ];
+    dbg!(&_stuff);
 }
 
 impl GroupThem for u8 {}
