5624040dfac3ce753a422d6ba19d479951a4a1ab
df@ <.@.>
2023-09-20 11:56:31 +0100

    OK#

--- generics/src/generic_nested_structs.rs
+++ generics/src/generic_nested_structs.rs
@@ -5,10 +5,15 @@ struct MainEntity {
 enum DataType {
     U8(Data<u8>),
     F32(Data<f32>),
+    Extra(ExtraStruct),
 }
 
 struct Data<T> {
     values: Vec<T>,
+}
+
+struct ExtraStruct {
+    values: Vec<String>,
 }
 
 impl<T> Data<T>
@@ -23,15 +28,25 @@ where
     }
 }
 
+impl ExtraStruct {
+    fn duplicate_and_print_for_strings(&self) {
+        for value in &self.values {
+            println!("extrastuct value: {}", value);
+        }
+    }
+}
+
 pub fn generic_nested_structs() {
     println!("generic_nested_structs");
     let row1 = DataType::U8(Data { values: vec![1_u8, 2_u8, 5_u8] });
     let row2 = DataType::F32(Data { values: vec![1.0_f32, 2.0_f32, 5.0_f32] });
-    let group = MainEntity { rows: vec![row1, row2] };
+    let row3 = ExtraStruct { values: vec!["alpha".to_string(), "beta".to_string() ]};
+    let group = MainEntity { rows: vec![row1, row2, DataType::Extra(row3)] };
     for row_values in group.rows {
         match row_values {
             DataType::U8(values) => values.duplicate_and_print(),
             DataType::F32(values) => values.duplicate_and_print(),
+            DataType::Extra(values) => values.duplicate_and_print_for_strings(),
         }
     }
 }
