ac2d671556840a7deddfa46be99455b223d7db6c
df@ <.@.>
2023-09-21 08:23:20 +0100

    ...

--- generics/src/generic_nested_structs_two.rs
+++ generics/src/generic_nested_structs_two.rs
@@ -2,15 +2,15 @@ struct MainEntity {
     rows: Vec<Data>,
 }
 
-struct Data<T> {
+struct Data {
     name: String,
-    values: Vec<T>,
+    values: Vec<u8>,
 }
 
 pub fn run() {
     println!("generic_nested_structs two");
     let mainentity = MainEntity { rows: vec![
         Data { name: "a".to_string(), values: vec![1_u8, 2_u8, 5_u8] },
-        Data { name: "b".to_string(), values: vec![1.0_f32, 2.0_f32, 5.0_f32] },
+        // Data { name: "b".to_string(), values: vec![1.0_f32, 2.0_f32, 5.0_f32] },
     ]};
 }
