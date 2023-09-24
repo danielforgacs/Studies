3106cb703d58cbb3caa264d71337e531e54ff1a3
df@ <.@.>
2023-09-24 23:34:02 +0100

    ok

--- generic_struct_w_traits/src/main.rs
+++ generic_struct_w_traits/src/main.rs
@@ -1,3 +1,30 @@
+trait Pixel {
+    fn to_chan(&self);
+}
+
+struct Exr<'a> {
+    channels: Vec<&'a dyn Pixel>,
+}
+
+#[derive(Debug, Clone)]
+struct Channel<T> {
+    name: String,
+    pixels: Vec<T>,
+}
+
+impl<T: std::fmt::Display + std::fmt::Debug> Pixel for Channel<T> {
+    fn to_chan(&self) {
+        for p in &self.pixels {
+            println!("{}, {:?}", p, p);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let chan_a = Channel { name: "a".to_string(), pixels: vec![1u8, 2u8, 3u8] };
+    let chan_b = Channel { name: "b".to_string(), pixels: vec![1.0f32, 2.0f32, 3.0f32] };
+    let exr = Exr { channels: vec![&chan_a, &chan_b] };
+
+    let ch = exr.channels[0].to_chan();
+    let ch = exr.channels[1].to_chan();
 }
