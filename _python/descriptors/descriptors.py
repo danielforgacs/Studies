e774e8ac62d3060ea1d8dbc19d5f77d834b3ab1f
df@ <.@.>
2024-08-14 07:56:11 +0100

    ok

--- _python/descriptors/descriptors.py
+++ _python/descriptors/descriptors.py
@@ -1,13 +1,19 @@
+class Meta(type):
+    pass
+
 class Descriptor:
+    def __set_name__(self, owner, name):
+        self.private_name = '_' + name
     def __set__(self, obj, value):
         if not isinstance(value, int):
             raise Exception('!!! THIS SHOULD HAVE BEEN AN int TYPE.')
-        obj.__dict__['attr'] = value
+        obj.__dict__[self.private_name] = value
     def __get__(self, obj, objtype):
-        return obj.__dict__['attr']
+        return obj.__dict__[self.private_name]
 
-class A:
+class A(metaclass=Meta):
     attr = Descriptor()
+    stuff = Descriptor()
 
 class B:
     attr = Descriptor()
@@ -28,6 +34,7 @@ if __name__ == '__main__':
     bbb.attr = 9876
     ccc.attr = 1111
     ddd.attr = 2222
+    aaa.stuff = 3
     assert aaa.attr == 5, '!!! GOT THE WRONG VALUE BACK.'
     assert bbb.attr == 9876, '!!! GOT THE WRONG VALUE BACK.'
     assert ccc.attr == 1111, '!!! GOT THE WRONG VALUE BACK.'
