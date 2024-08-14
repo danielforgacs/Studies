f502a455da2566227e65aeaf2feee047535c4af5
df@ <.@.>
2024-08-14 07:01:00 +0100

    json custom

--- _python/custom_json_encoding/jsoncustom.py
+++ _python/custom_json_encoding/jsoncustom.py
@@ -1,24 +1,13 @@
 import json
 
 
-# Custom object
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class A:
+    pass
+    def as_dict(self):
+        return {'a': 1}
 
 
-# Custom encoder for Person objects
-class CustomEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Person):
-            return {"name": obj.name, "age": obj.age}
 
-        return super().default(obj)
-
-# Sample custom object
-person_obj = Person("Alice", 25)
-
-# Encoding custom object using the custom encoder
-json_string = json.dumps(person_obj, cls=CustomEncoder)
-print(json_string)  # Output: {"name": "Alice", "age": 25}
+a = A()
+data = json.dumps(a.as_dict())
+print(data)
