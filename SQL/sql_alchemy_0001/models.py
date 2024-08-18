842b28f70f25ec00922d1362cf589a75e8ac98b6
df@ <.@.>
2024-08-18 15:28:59 +0100

    ok

--- SQL/sql_alchemy_0001/models.py
+++ SQL/sql_alchemy_0001/models.py
@@ -1,6 +1,7 @@
 import connection
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String
 
 class Base(DeclarativeBase):
     pass
@@ -10,9 +11,11 @@ class Shit(Base):
     __tablename__ = 'shit'
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    name: orm.Mapped[str] = orm.mapped_column(String(30))
 
 
 def migrate():
+    print('::migrating')
     Base.metadata.create_all(connection.engine)
 
 
