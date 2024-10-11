c2d53f75bc42c7b62918523d9f8ad5b1d60c5183
df@ <.@.>
2024-10-11 18:43:08 +0100

    ok

---
+++ __rnd/name_check_regex/rename_test.py
@@ -0,0 +1,20 @@
+import pytest
+import re
+
+PATTERN = r'((a+)+)(_((a+)*))*'
+PATTERN = r'((a+)+)(_(a+))*'
+
+def name_check(input):
+    return bool(re.fullmatch(PATTERN, input))
+
+
+@pytest.mark.parametrize('expected, input', (
+    (True, 'a'),
+    (True, 'aaa'),
+    (True, 'aaa_a'),
+
+    (False, '_'),
+    (False, '_a'),
+))
+def test_name_regex(expected, input):
+    assert name_check(input) is expected
