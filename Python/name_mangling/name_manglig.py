da191d737130d3ae59c7d7c9b762ba4f0f5a0da2
df@ <.@.>
2025-05-15 20:44:09 +0100

    ok

--- Python/name_mangling/name_manglig.py
+++ Python/name_mangling/name_manglig.py
@@ -1,9 +1,32 @@
 class K:
     def __init__(self):
-        self.attr = 1234
-        self._attr_2 = 1234
-        self.__attr_3 = 1234
+        self.attr = 'plain attr'
+        self._attr_2 = 'private attr'
+        self.__attr_3 = 'name mangled attr'
+
+    # @property
+    # def _attr_2(self):
+    #     return self._attr_2
+
+    # @_attr_2.setter
+    # def _attr_2(self, value):
+    #     pass
+    #     # self._attr_2 = value
+
+
+    @property
+    def _attr_3(self):
+        return self.__attr_3
 
 
 i = K()
 print(dir(i))
+print(i.attr)
+print(i._attr_2)
+print(i._attr_3)
+try:
+    print(i.__attr_3)
+except AttributeError:
+    pass
+
+print(i._K__attr_3)
